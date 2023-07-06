2281a120718d75d57620c2ab745442a5061ad99f
peterlidee <peter@lidee.be>
2023-07-06 21:16:03 +0200

    Updated type predicate

--- lib/isValidSortOrder.ts
+++ lib/isValidSortOrder.ts
@@ -3,6 +3,6 @@ import { SortOrder } from '@/types/SortOrder';
 // type predicate
 // checks if value is valid SortBy type
 export default function isValidSortOrder(value: string): value is SortOrder {
-  const validOptions: [SortOrder, SortOrder] = ['asc', 'desc'];
+  const validOptions: [SortOrder[0], SortOrder[1]] = ['asc', 'desc'];
   return validOptions.includes(value as SortOrder);
 }
