67fd6d2bd4942c1933fce1829c19057a3ffd0ebb
peterlidee <peter@lidee.be>
2023-07-09 12:18:03 +0200

    Renamed file and function

--- lib/validateSortOrder.ts
+++ lib/validateSortOrder.ts
@@ -1,8 +1,8 @@
-import { SortOrder } from '@/types/SortOrder';
+import { SortOrder } from '@/types';
 
 // type predicate
 // checks if value is valid SortBy type
-export default function isValidSortOrder(value: string): value is SortOrder {
+export default function validateSortOrder(value: string): value is SortOrder {
   const validOptions: [SortOrder[0], SortOrder[1]] = ['asc', 'desc'];
   return validOptions.includes(value as SortOrder);
 }
