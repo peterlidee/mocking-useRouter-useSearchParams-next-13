0f5994aa00b5f83b6c5aa34c4d23d366a24733cd
peterlidee <peter@lidee.be>
2023-07-09 14:44:16 +0200

    Updated List + test

--- components/List.tsx
+++ components/List.tsx
@@ -1,12 +1,11 @@
-import { SortOrder } from '@/types/SortOrder';
-
-const list = ['Banana', 'Apple', 'Lemon', 'Cherry'];
+import { SortOrder } from '@/types';
 
 type Props = {
   sortOrder: SortOrder;
 };
 
 export default function List({ sortOrder }: Props) {
+  const list = ['Banana', 'Apple', 'Lemon', 'Cherry'];
   const sortedList = [...list].sort((a, b) => {
     if (sortOrder === 'asc') {
       return a > b ? 1 : -1;
